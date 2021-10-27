ee99964f547456300181da64f88e208281232a19
gyalpo <gyalpodongo@gmail.com>
2021-10-27 17:42:51 -0400

    Added

--- kraken/kraken_bot.py
+++ kraken/kraken_bot.py
@@ -3,9 +3,20 @@ import krakenex
 from data import kraken_api_key, kraken_api_private_key
 
 class KrakenBot:
-    def __init__(self):
-        return
+    def __init__(self,currency):
+        self.bot = krakenex.API(key = kraken_api_key, secret = kraken_api_private_key)
+        self.currency = currency 
+    
+    def check_balance(self):
+        try:
+            result = self.bot.query_private('Balance')
+        except:
+            print("Error")
+        if 'result' in result:
+            return result['result']
+        else:
+            return result['error']
     
 if __name__ == "__main__":
-    k = krakenex.API(key = kraken_api_key, secret = kraken_api_private_key)
-    print(k.query_private('Balance'))
+    k = KrakenBot()
+    print(k.check_balance())
