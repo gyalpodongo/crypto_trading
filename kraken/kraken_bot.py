533858e39f11358ebdcc23b461706ad5d0109760
gyalpo <gyalpodongo@gmail.com>
2021-10-28 22:21:42 -0400

    Started with another api

--- kraken/kraken_bot.py
+++ kraken/kraken_bot.py
@@ -1,11 +1,20 @@
 import krakenex
+import time
 #from pykrakenapi import KrakenAPI
 from data import kraken_api_key, kraken_api_private_key
 
 class KrakenBot:
-    def __init__(self,currency):
+    def __init__(self,currency="USD"):
         self.bot = krakenex.API(key = kraken_api_key, secret = kraken_api_private_key)
         self.currency = currency 
+
+    def buy(self,data):
+        return self.bot.query_private('AddOrder',data)
+    def assetInfo(self):
+        return self.bot.query_public("Assets")['result']
+    def ticker(self,pair):
+        return self.bot.query_public(f"Ticker?pair={pair}")
+    
     
     def check_balance(self):
         try:
@@ -20,3 +29,5 @@ class KrakenBot:
 if __name__ == "__main__":
     k = KrakenBot()
     print(k.check_balance())
+    print(k.assetInfo()['XXBT'])
+    print(k.ticker("XBTGBP"))
